5b229210057f0202d4f91a2d151907d2450c249c
Ashton Monzi <anmonzi@gmail.com>
2021-04-20 12:09:31 -0500

    created function that views yearly seed plan and returns an array of food types to be planted

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,36 +1,23 @@
 import { createPlan } from "./plan.js"
-import { createAsparagus } from "./seeds/asparagus.js"
-import { createCorn } from "./seeds/corn.js"
-import { createPotato } from "./seeds/potato.js"
-import { createSoybean } from "./seeds/soybean.js"
-import { createSunflower } from "./seeds/sunflower.js"
-import { createWheat } from "./seeds/wheat.js"
+
 import { addPlant, usePlants } from "./field.js"
+import { plantSeeds } from "./tractor.js"
 
 
 const yearlyPlan = createPlan()
 
-const asparagusSeed = createAsparagus()
-const cornSeed = createCorn()
-const potatoSeed = createPotato()
-const soybeanSeed = createSoybean()
-const sunflowerSeed = createSunflower()
-const wheatSeed = createWheat()
+const seedsPlanted = plantSeeds(yearlyPlan)
 
-addPlant(cornSeed)
-addPlant(asparagusSeed)
-const showUsedPlants = usePlants()
+
+// addPlant(potatoSeed)
+// addPlant(asparagusSeed)
+// addPlant(wheatSeed)
+// addPlant(cornSeed)
+// const showPlantsInField = usePlants()
+
 
 
 
 
 console.log(yearlyPlan)
-
-console.log(asparagusSeed)
-console.log(cornSeed)
-console.log(potatoSeed)
-console.log(soybeanSeed)
-console.log(sunflowerSeed)
-console.log(wheatSeed)
-
-console.log(showUsedPlants)
+console.log(seedsPlanted)
